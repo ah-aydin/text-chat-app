5d6d8fb9732ecd7c7fcd0645ae8a7fba53a04f10
Ahmed Hamza Aydin <ahamzaaydin.py@gmail.com>
2022-01-31 19:23:34 +0100

    Update asgi.py
    In order to avoid INSTALLED APP comlicatoins

--- core/asgi.py
+++ core/asgi.py
@@ -3,9 +3,10 @@ import os
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-import chat.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
+import chat.routing
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
